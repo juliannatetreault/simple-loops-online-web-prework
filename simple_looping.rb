22d343cab25de48c38661cc3182d11427a4c1df3
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-10 04:00:00 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -33,7 +33,7 @@ def until_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
-  
+  until counter 
 
 end
 
