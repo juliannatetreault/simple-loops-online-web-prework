161865fba049319b39ef7915410458befa6cfd26
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-10 03:49:47 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,7 +1,7 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-  counter = 7
+  counter = 0
   loop do
     counter +=1
     phrase = "Welcome to Flatiron School's Web Development Course!"
